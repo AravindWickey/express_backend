c871d408dad63c6a5afd5f553553de67dd85835f
Aravind Wickey <aravind261998@gmail.com>
2022-01-11 20:21:44 +0530

    commit

--- models/roomDetailsModel.js
+++ models/roomDetailsModel.js
@@ -1,7 +1,32 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const roomDetailsSchema = new Schema({});
+const roomDetailsSchema = new Schema({
+  room_no: {
+    type: Number,
+    required: true,
+  },
+  number_of_seats: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  room_status: {
+    type: String,
+    minLength: 5,
+    maxLength: 250,
+    required: true,
+  },
+  amenities: {
+    type: String,
+    minLength: 5,
+    maxLength: 250,
+    required: true,
+  },
+});
 
 const roomDetailsModel = mongoose.model(
   "roomDetailsModel",
