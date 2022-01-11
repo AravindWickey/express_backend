35b1f9746d930fe0340ecf7fa3dda8cded83a0d9
Aravind Wickey <aravind261998@gmail.com>
2022-01-11 21:11:06 +0530

    commit

--- module/roomDetailsModule.js
+++ module/roomDetailsModule.js
@@ -1,14 +1,12 @@
-const mongo = require('../shared/connect');
 const roomDetailsVar = require("../models/roomDetailsModel");
 const { ObjectId } = require("bson");
 
 module.exports.getroomDetails = async (req, res, next) => {
   try {
-    var data = await roomDetailsVar.find().toArray();
+    var data = await roomDetailsVar.find();
     res.send(data);
   } catch {
     console.log(err);
     res.status(500).send(err);
   }
 };
-
